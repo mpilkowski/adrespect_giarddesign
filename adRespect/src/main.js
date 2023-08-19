c27a3be338beff28e498bbbd3276876024b58fe1
Szczenathemonke <mpilkowski.learn@gmail.com>
2023-08-19 20:14:09 +0200

    update-layout
    1440px figma layout
    masonry added
    animated offers added

--- adRespect/src/main.js
+++ adRespect/src/main.js
@@ -4,4 +4,27 @@
 //   </div>
 // `;
 
-setupCounter(document.querySelector("#counter"));
+import Masonry from "masonry-layout";
+
+//1. animated offers
+const hiddenElements = document.querySelectorAll(".hide");
+const observer = new IntersectionObserver((element) => {
+  element.forEach((enter) => {
+    if (enter.isIntersecting) {
+      enter.target.classList.add("show");
+    } else {
+      enter.target.classList.remove("show");
+    }
+  });
+});
+
+hiddenElements.forEach((e) => observer.observe(e));
+//2. Gallery with masonry
+
+let gallery = document.querySelector(".gridMsnr");
+let galleryReel = new Masonry(gallery, {
+  itemSelector: ".gridMsnr-item",
+  columnWidth: 451,
+  gutter: 43,
+  horizontalOrder: true,
+});

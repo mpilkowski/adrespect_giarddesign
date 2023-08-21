490f3b21821a99a1917525b196109362e2d7f2dc
Szczenathemonke <mpilkowski.learn@gmail.com>
2023-08-21 14:56:03 +0200

    update
    todo:
    
    search
    showmore (gallery)
    heroSlider

--- adRespect/src/main.js
+++ adRespect/src/main.js
@@ -44,77 +44,32 @@ document.querySelector(".popup span").addEventListener("click", () => {
 
 // 4.smooth scroll effect
 
-// document.querySelectorAll(".scrollLink").forEach((a) => {
-//   a.onclick = (e) => {
-//     let destination = a.getAttribute("href");
-//     let navElement = document.getElementById(
-//       destination.slice(1, destination.length).toString()
-//     );
-//     e.preventDefault();
+document.querySelectorAll(".scrollLink").forEach((a) => {
+  a.onclick = (e) => {
+    let destination = a.getAttribute("href");
+    let navElement = document.getElementById(
+      destination.slice(1, destination.length).toString()
+    );
+    e.preventDefault();
 
-//     navElement &&
-//       navElement.scrollIntoView({ behavior: "smooth", block: "center" });
-//   };
-// });
+    navElement &&
+      navElement.scrollIntoView({ behavior: "smooth", block: "center" });
+  };
+});
 // 5.dropDown menu "oferty"
-
-// document.querySelector(".dropDownBtn").addEventListener("click", () => {
-//   document.querySelector(".dropDownContent").classList.toggle("showDropDown");
-// });
-// document.documentElement.addEventListener("click", () => {
-//   if (
-//     document
-//       .querySelector(".dropDownContent")
-//       .classList.contains("showDropDown")
-//   ) {
-//     document.querySelector(".dropDownContent").style.display = "none";
-//   }
-// });
-
-// window.onclick = () => {
-//   if (document.querySelector(".dropDownContent").style.display === "block") {
-//     document.querySelector(".dropDownContent").style.display = "none";
-//   }
-// };
 
 const dropDownBtn = document.querySelector(".dropDownBtn");
 const dropDownContent = document.querySelector(".dropDownContent");
-const openDropDown = () => {
-  dropDownContent.classList.toggle("showDropDown");
-  console.log("boom");
-};
 
-dropDownBtn.addEventListener("click", openDropDown());
+dropDownBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  dropDownContent.classList.add("showDropDown");
+});
 document.documentElement.addEventListener("click", () => {
   if (dropDownContent.classList.contains("showDropDown")) {
-    openDropDown();
+    dropDownContent.classList.remove("showDropDown");
   }
 });
-// const dropdownBtn = document.getElementById("btn");
-// const dropdownMenu = document.getElementById("dropdown");
-// const toggleArrow = document.getElementById("arrow");
-
-// const toggleDropdown = function () {
-//   dropdownMenu.classList.toggle("show");
-//   toggleArrow.classList.toggle("arrow");
-// };
-
-// dropdownBtn.addEventListener("click", function (e) {
-//   // e.stopPropagation();
-//   toggleDropdown();
-// });
-
-// document.documentElement.addEventListener("click", function () {
-//   if (dropdownMenu.classList.contains("show")) {
-//     toggleDropdown();
-//   }
-// });
-
-// document.querySelector(".dropDownBtn").addEventListener("click", () => {
-//   document.querySelector(".dropDownContent").style.display = "block";
-// });
-// document.documentElement.addEventListener("click", () => {
-//   if (document.querySelector(".dropDownContent").style.display === "block") {
-//     document.querySelector(".dropDownContent").style.display = "none";
-//   }
-// });
+// 6. Gallery
+const msnrGallery = document.getElementById("Msnr");
+const msnrBtn = document.getElementById("MsnrBtn");

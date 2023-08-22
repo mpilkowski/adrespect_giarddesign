1d0b7079b1a2a4440f2c3153eb5a4fe62b85a1b2
Szczenathemonke <mpilkowski.learn@gmail.com>
2023-08-22 14:04:35 +0200

    final version

--- adRespect/src/main.js
+++ adRespect/src/main.js
@@ -7,7 +7,7 @@
 import Masonry from "masonry-layout";
 
 //1. animated offers
-const hiddenElements = document.querySelectorAll(".hide");
+const hiddenElements = document.querySelectorAll(".cards .hide");
 const observer = new IntersectionObserver((element) => {
   element.forEach((enter) => {
     if (enter.isIntersecting) {
@@ -27,20 +27,59 @@ let galleryReel = new Masonry(gallery, {
   columnWidth: 451,
   gutter: 43,
   horizontalOrder: true,
+  fitWidth: true,
 });
 
-//3. popup Gallery
-document.querySelectorAll(".gridMsnr .gridMsnr-item img").forEach((img) => {
-  img.onclick = () => {
-    document.querySelector(".popup").style.display = "block";
-    document.querySelector(".popup img").src = img.getAttribute("src");
-    document.getElementById("Navi").style.position = "unset";
-  };
+let msnrGallery = document.getElementById("Msnr");
+let msnrBtnOpen = document.getElementById("MsnrBtnOpen");
+let msnrBtnClose = document.getElementById("MsnrBtnClose");
+let msnrGradient = document.getElementById("MsnrGradient");
+
+msnrBtnOpen.addEventListener("click", () => {
+  for (let photoIndex = 1; photoIndex < 10; photoIndex++) {
+    let newBrick = document.createElement("div");
+    newBrick.classList += "gridMsnr-item gridMsnr-w1 gridMsnr-h3";
+    newBrick.innerHTML = `<img src="./src/assets//gallery/GalleryPhoto${photoIndex}.png" alt="Photo${photoIndex}" />`;
+    msnrGallery.appendChild(newBrick);
+    galleryReel.appended(newBrick);
+  }
+  msnrBtnOpen;
+  popupGallery();
+
+  msnrBtnOpen.style.display = "none";
+  msnrBtnClose.style.display = "block";
+  msnrGradient.style.display = "none";
+
+  galleryReel.reloadItems();
 });
-document.querySelector(".popup span").addEventListener("click", () => {
-  document.querySelector(".popup").style.display = "none";
-  document.getElementById("Navi").style.position = "sticky";
+msnrBtnClose.addEventListener("click", () => {
+  for (let photoIndex = 1; photoIndex < 10; photoIndex++) {
+    msnrGallery.removeChild(msnrGallery.lastChild);
+    galleryReel.remove(msnrGallery.lastChild);
+  }
+
+  msnrBtnOpen.style.display = "block";
+  msnrBtnClose.style.display = "none";
+  msnrGradient.style.display = "block";
+
+  galleryReel.reloadItems();
 });
+
+//3. popup for Gallery
+function popupGallery() {
+  document.querySelectorAll(".gridMsnr .gridMsnr-item img").forEach((img) => {
+    img.onclick = () => {
+      document.querySelector(".popup").style.display = "block";
+      document.querySelector(".popup img").src = img.getAttribute("src");
+      document.getElementById("Navi").style.position = "unset";
+    };
+  });
+  document.querySelector(".popup span").addEventListener("click", () => {
+    document.querySelector(".popup").style.display = "none";
+    document.getElementById("Navi").style.position = "sticky";
+  });
+}
+popupGallery();
 
 // 4.smooth scroll effect
 
@@ -70,6 +109,51 @@ document.documentElement.addEventListener("click", () => {
     dropDownContent.classList.remove("showDropDown");
   }
 });
-// 6. Gallery
-const msnrGallery = document.getElementById("Msnr");
-const msnrBtn = document.getElementById("MsnrBtn");
+
+const searchBtn = document.querySelector(".searchBtn");
+const searchContent = document.querySelector(".searchContent");
+const searchSubmit = document.getElementById("searchSubmit");
+
+searchBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  searchContent.classList.add("showDropDown");
+});
+searchSubmit.addEventListener("click", () => {
+  console.log(1);
+  if (searchContent.classList.contains("showDropDown")) {
+    searchContent.classList.remove("showDropDown");
+  }
+});
+
+// 6. Hero Slider
+
+let sliderIndex = 1;
+let next = document.querySelector(".nextBtn");
+let back = document.querySelector(".backBtn");
+
+next.addEventListener("click", () => {
+  sliderIndex += 1;
+  showSlide();
+});
+
+back.addEventListener("click", () => {
+  sliderIndex -= 1;
+  showSlide(sliderIndex);
+});
+
+function showSlide() {
+  let slides = document.querySelectorAll(".slide");
+
+  if (sliderIndex > slides.length) {
+    sliderIndex = 1;
+  }
+  if (sliderIndex < 1) {
+    sliderIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[sliderIndex - 1].style.display = "block";
+}
+showSlide();
